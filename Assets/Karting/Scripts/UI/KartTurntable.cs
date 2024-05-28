a59407b53759b3077203f955932a440d5ab72ba2
Christian Ted Ochoa <christianted.ochoa@gmail.com>
2024-05-28 16:01:55 +0800

    Draft the main menu scene

---
+++ Assets/Karting/Scripts/UI/KartTurntable.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class KartTurntable : MonoBehaviour
+{
+    [SerializeField]
+    private float rotationSpeed = 0.25f;
+
+    private void Update()
+    {
+        Rotate();
+    }
+
+    private void Rotate()
+    {
+        transform.Rotate(Vector3.up * rotationSpeed);
+    }
+}
