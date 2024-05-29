1adf23cac7cec6ee6f657ba1ef21d232a434f755
Prodejay <47100935+Prodejay@users.noreply.github.com>
2024-05-29 14:56:30 +0800

    [*] Created coin counter

--- Assets/Karting/Prefabs/Coins/CoinCollectionManager.cs
+++ Assets/Karting/Prefabs/Coins/CoinCollectionManager.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 namespace KartGame.Items
@@ -8,10 +9,13 @@ namespace KartGame.Items
     {
         [SerializeField]
         private int m_coinCount;
+        [SerializeField]
+        private TextMeshProUGUI m_coinCountText;
 
         public void AddCoin()
         {
             m_coinCount++;
+            m_coinCountText.text = m_coinCount.ToString();
         }
     }
 }
