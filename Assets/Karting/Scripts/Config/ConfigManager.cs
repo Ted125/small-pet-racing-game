b54ecaa1482c3b2c4ae27363c528e5326602a94c
Christian Ted Ochoa <christianted.ochoa@gmail.com>
2024-05-29 15:56:24 +0800

    Implement the energy system

---
+++ Assets/Karting/Scripts/Config/ConfigManager.cs
@@ -0,0 +1,12 @@
+using Karting.Utilities;
+using UnityEngine;
+
+namespace Karting.Config
+{
+    public class ConfigManager : PersistentSingleton<ConfigManager>
+    {
+        [SerializeField] private EnergyConfig _energyConfig;
+
+        public EnergyConfig EnergyConfig { get { return _energyConfig; } }
+    }
+}
